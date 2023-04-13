f6cf4e2b6657811d00f747e4b49997ab50f51482
zaranc <130464977+zaranc@users.noreply.github.com>
2023-04-13 18:21:48 +0530

    Add files via upload

---
+++ gn0c.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	printf("\t*   *   *   *   *   *   *");
+	printf("\n\n\n");
+	printf("*\t*   *   *   *   *   *   *\n\n");
+	printf("*           *                   *\n\n");
+	printf("*           *       *   *   *   *\n\n");
+	printf("*           *       *       *   *\n\n");
+	printf("*        *  *   *   *   *   *   *");
+	
+}
---
+++ gn1c.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	printf("\t *       *\n");     
+	printf("     *       *\n");
+	printf("*\t *\n");
+	printf("     *       *\n");
+	printf("\t *       *\n");
+	printf("\n");
+	printf("\n");
+	printf("           (I)");
+	
+	
+	
+	
+	
+	
+}
